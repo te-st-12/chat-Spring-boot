9f1a1ac392250cb7e603d1fce363d7bab5e15c6d
Noor-se <fackfb506@gmail.com>
2025-09-07 19:57:30 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,6 @@ dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("com.mysql:mysql-connector-j:9.4.0")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("mysql:mysql-connector-java:8.0.33")
