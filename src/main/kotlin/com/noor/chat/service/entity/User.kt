db56b331a4a35463c914f868344bd97934b32fc7
Noor-se <fackfb506@gmail.com>
2025-09-12 04:00:39 +0300

    Update User.kt

--- src/main/kotlin/com/noor/chat/service/entity/User.kt
+++ src/main/kotlin/com/noor/chat/service/entity/User.kt
@@ -1,14 +1,16 @@
 package com.noor.chat.service.entity
 
-import com.fasterxml.jackson.annotation.JsonFormat
+import java.util.Date
 import jakarta.persistence.*
-import java.sql.Date
-import java.util.UUID
 
 @Entity
 @Table(name = "users")
 data class User(
-    @Id val id: UUID, val name: String,
+    @Id
+    val id: UUID,
+
+    val name: String,
+
     @Temporal(TemporalType.TIMESTAMP)
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
-    val insertedDate: Date = Date(System.currentTimeMillis()))
+    val insertedDate: Date = Date(System.currentTimeMillis())
+)
