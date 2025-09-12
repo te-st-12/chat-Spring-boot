21a65ebd579f367f5f20a97f60930378ce6a73d9
Noor Serry <nour.serry.94@gmail.com>
2025-09-12 03:47:58 +0300

    Add User `insertedDate` and POST endpoint
    This commit introduces an `insertedDate` field to the `User` entity, automatically populated with the current timestamp upon creation.
    
    It also adds a new POST endpoint to `UserController` and a corresponding `addUsers` method in `UserService` to allow for adding multiple users.

--- src/main/kotlin/com/noor/chat/service/entity/User.kt
+++ src/main/kotlin/com/noor/chat/service/entity/User.kt
@@ -1,5 +1,6 @@
 package com.noor.chat.service.entity
 
+import com.fasterxml.jackson.annotation.JsonFormat
 import jakarta.persistence.*
 import java.sql.Date
 import java.util.UUID
@@ -8,5 +9,6 @@ import java.util.UUID
 @Table(name = "users")
 data class User(
     @Id val id: UUID, val name: String,
-    val insertedDate: Date = Date(System.currentTimeMillis())
-)
+    @Temporal(TemporalType.TIMESTAMP)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    val insertedDate: Date = Date(System.currentTimeMillis()))
