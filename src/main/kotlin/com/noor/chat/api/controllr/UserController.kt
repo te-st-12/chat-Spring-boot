6cc4ad3c2a23d86a5df4c022bbe51109f45f7ef7
Noor-se <fackfb506@gmail.com>
2025-09-13 22:49:13 +0300

    Update UserController.kt

--- src/main/kotlin/com/noor/chat/api/controllr/UserController.kt
+++ src/main/kotlin/com/noor/chat/api/controllr/UserController.kt
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.util.UUID
+import org.springframework.web.bind.annotation.DeleteMapping
 
 @RestController
 @RequestMapping("/users")
